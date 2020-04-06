81592dca32150606f6197e825433cc9fdb92991e
Jon Latane <jonlatane@gmail.com>
2020-04-06 15:56:44 -0400

    app updates

--- app-staging/flutter_service_worker.js
+++ app-staging/flutter_service_worker.js
@@ -231,7 +231,7 @@ const RESOURCES = {
 "MIDI.js/LICENSE.txt": "c22fc40d350cad44a54887315bcd5f74",
 "index.html": "de19091dd2a982f2beccd2cf304180de",
 "/": "de19091dd2a982f2beccd2cf304180de",
-"main.dart.js": "f75979af1ff0a59780505004bcf6a527",
+"main.dart.js": "c008ebfefb3efc3fa11ca8bee607058d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
