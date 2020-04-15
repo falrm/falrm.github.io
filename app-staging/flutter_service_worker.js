bb3b9fc003e574df44fb7e18d4c3eb6a7022048e
Jon Latane <jonlatane@gmail.com>
2020-04-15 18:38:57 -0400

    fix web pushPart

--- app-staging/flutter_service_worker.js
+++ app-staging/flutter_service_worker.js
@@ -231,7 +231,7 @@ const RESOURCES = {
 "MIDI.js/LICENSE.txt": "c22fc40d350cad44a54887315bcd5f74",
 "index.html": "60992ad2713161384e2d2a71bf17852a",
 "/": "60992ad2713161384e2d2a71bf17852a",
-"main.dart.js": "99581e828180c9ede137b9e25769286c",
+"main.dart.js": "2a52c3778a4675c553af2086ea916fd0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
