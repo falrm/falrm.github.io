52aa2ecaa5f52fff7bc3d809d7173cc6c7962fb0
Jon Latane <jonlatane@gmail.com>
2020-05-07 09:59:29 -0400

    canvaskit web app

--- app-staging/flutter_service_worker.js
+++ app-staging/flutter_service_worker.js
@@ -233,7 +233,7 @@ const RESOURCES = {
 "MIDI.js/LICENSE.txt": "c22fc40d350cad44a54887315bcd5f74",
 "index.html": "ea32ce02e810897bce681c46bf99cb18",
 "/": "ea32ce02e810897bce681c46bf99cb18",
-"main.dart.js": "60a899e67e73e2cd81da269f53ef0135",
+"main.dart.js": "0c597fff62ae04ab51e5d85c28e56a14",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
